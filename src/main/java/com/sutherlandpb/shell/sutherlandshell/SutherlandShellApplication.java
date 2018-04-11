2b5930a080eb1d577b18b78855dc8d75b66cf55d
unknown <sutherlandpb@gmail.com>
2018-04-10 22:03:52 -0400

    initial maven project

---
+++ src/main/java/com/sutherlandpb/shell/sutherlandshell/SutherlandShellApplication.java
@@ -0,0 +1,12 @@
+package com.sutherlandpb.shell.sutherlandshell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SutherlandShellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SutherlandShellApplication.class, args);
+	}
+}
